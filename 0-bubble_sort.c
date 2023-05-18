4d1c862a839986cd0afa58ec5b4782db668b05f0
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-18 08:05:04 -0700

    Fixed Indentation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,18 +9,18 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;          /* Iterator */
-    bool unsorted = 1; /* Checks if array remains unsorted */
+	size_t i;		   /* Iterator */
+	bool unsorted = 1; /* Checks if array remains unsorted */
 
-    while (unsorted != 0)
-    {
-        unsorted = 0;
-        for (i = 0; i < size; i++)
-            if (i < size - 1 && array[i] > array[i + 1])
-            {
-                swap(array, i);
-                unsorted = 1;
-                print_array(array, size);
-            }
-    }
+	while (unsorted != 0)
+	{
+		unsorted = 0;
+		for (i = 0; i < size; i++)
+			if (i < size - 1 && array[i] > array[i + 1])
+			{
+				swap(array, i);
+				unsorted = 1;
+				print_array(array, size);
+			}
+	}
 }
