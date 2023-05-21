585621a19ec91ab5694edbd2d10ab8e2a25e0f80
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-21 06:21:28 -0700

    Changed Swap Func Dependency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
-#include "swap.c"
+
+/**
+ * swap - swaps two elements in array
+ * @arr1: first element of array
+ * @arr2: second element of array
+ */
+
+void swap(int *array, int i)
+{
+
+	int temp = array[i];
+
+	array[i] = array[i + 1];
+	array[i + 1] = temp;
+}
 
 /**
  * bubble_sort - sorts array
