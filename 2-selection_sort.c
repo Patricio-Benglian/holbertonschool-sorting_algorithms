9e3cf34350092e3fcc6f86c21132f293ed605536
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-22 06:19:13 -0700

    Selection Sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t a, b; /* Iterators */
+	size_t min;	 /* Index of smallest value */
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (a = 0; a < size; a++)
+	{
+		min = a;
+		for (b = a + 1; b < size; b++)
+		{
+			if (array[b] < array[min])
+				min = b; /* Save index of smallest value */
+		}
+		if (array[min] != array[a])
+		{
+			temp = array[a];
+			array[a] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
